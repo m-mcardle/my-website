5cc8f239078b5c0bde17da3b920324b8c44cae9c
Matt McArdle <matthew.j.mcardle@gmail.com>
2022-07-01 13:00:02 -0400

    Add wave / scroll animations

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,14 @@
   <div class="main bg-black text-white h-screen">
     <div class="main w-full flex flex-row">
       <div class="intro flex flex-col p-16 ml-0 mr-auto">
-        <h2 class="py-8">
-          Hi there! 👋
-        </h2>
+        <div class="greeting flex flex-row py-8">
+          <h2>
+            Hi there!&nbsp;
+          </h2>
+          <div class="wave">
+            <h2>👋</h2>
+          </div>
+        </div>
         <h3>My name is</h3>
         <h1>Matt McArdle</h1>
         <hr>
@@ -14,13 +19,16 @@
         <img class="w-64 h-64" alt="Matt McArdle" src="~/assets/images/MatthewMcArdleIcon.jpg">
       </div>
     </div>
-    <div class="bottom-0 absolute p-16 w-full">
+    <div class="bottom-0 absolute p-16 pb-0 w-full">
       <h3>Come read about my <b>skills</b>, <b>experiences</b>, and <b>interests</b>!</h3>
       <hr>
       <div class="logo-images flex flex-row justify-between">
         <img class="w-32 h-32" alt="University of Guelph Logo" src="~/assets/images/UoG-Logo.jpeg">
         <img class="w-32 h-32" alt="Magnet Forensics Logo" src="~/assets/images/Magnet-Logo.png">
         <img class="w-32 h-32" alt="Vidyard Logo" src="~/assets/images/Vidyard-Logo.png">
+      </div>
+      <div class="w-full">
+        <ChevronDownSolid class="scroll-icon w-32 h-32 mx-auto" />
       </div>
     </div>
   </div>
@@ -29,8 +37,15 @@
 <script lang="ts">
 import Vue from 'vue'
 
+// @ts-ignore
+import { ChevronDownSolid } from 'vue-icon-packs/hi'
+
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  components: {
+    ChevronDownSolid
+  }
 })
 </script>
 
@@ -58,5 +73,37 @@ export default Vue.extend({
 
   img {
     background-color: white;
+  }
+
+  @keyframes waving {
+    0% {
+      transform: rotate(-20deg);
+    }
+    50% {
+      transform: rotate(20deg);
+    }
+    100% {
+      transform: rotate(-20deg);
+    }
+  }
+
+  @keyframes blink {
+    0% {
+      opacity: 0%;
+    }
+    50% {
+      opacity: 100%;
+    }
+    100% {
+      opacity: 0%;
+    }
+  }
+
+  .wave {
+    animation: waving 1.5s infinite linear;
+  }
+
+  .scroll-icon {
+    animation: blink 2s infinite linear;
   }
 </style>
