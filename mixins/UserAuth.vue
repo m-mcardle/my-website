2a60d04089aa7a88aa7da78d0edd9796133d3898
Matt McArdle <matthew.j.mcardle@gmail.com>
2022-07-26 21:20:18 -0400

    Use @nuxtjs/toast for Toasts

--- mixins/UserAuth.vue
+++ mixins/UserAuth.vue
@@ -19,35 +19,54 @@ export default (Vue as VueConstructor<Vue>).extend({
     }
   },
 
+  created () {
+    this.isUserAdmin()
+  },
+
   methods: {
     async login (email: string, password: string): Promise<void> {
+      const loadingToast = this.$toast.show('Logging in..')
       try {
         await this.$fire.auth.signInWithEmailAndPassword(email, password)
+        this.$toast.success('Sucessfully Logged In!')
         this.userError = ''
       } catch (ex) {
         this.userError = ex
+        this.$toast.error('Error while authenticating')
+      } finally {
+        loadingToast.goAway(0)
       }
 
       this.user = this.$fire.auth.currentUser
     },
 
     async googleLogin (): Promise<void> {
+      const loadingToast = this.$toast.show('Logging in with Google...')
       try {
         await this.$fire.auth.signInWithPopup(this.provider)
+        this.$toast.success('Sucessfully Logged In!')
         this.userError = ''
       } catch (ex) {
         this.userError = ex
+        this.$toast.error('Error while authenticating')
+      } finally {
+        loadingToast.goAway(0)
       }
 
       this.user = this.$fire.auth.currentUser
     },
 
     async signOut (): Promise<void> {
+      const loadingToast = this.$toast.show('Logging out...')
       try {
         await this.$fire.auth.signOut()
+        this.$toast.success('Sucessfully Logged Out!')
         this.userError = ''
       } catch (e) {
         this.userError = e
+        this.$toast.error('Error while signing out')
+      } finally {
+        loadingToast.goAway(0)
       }
 
       this.user = this.$fire.auth.currentUser
