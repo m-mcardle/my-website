806fbc075e1508e1c53abd7780cb40fbebc05a94
Matt McArdle <matthew.j.mcardle@gmail.com>
2022-08-07 14:10:39 -0400

    Change to use `Raleway` font (#11)

--- pages/index.vue
+++ pages/index.vue
@@ -35,14 +35,6 @@ export default Vue.extend({
     InfoSection,
     TimelineSection,
     NavHeader
-  },
-
-  data () {
-    return {
-      lgImageClass: 'w-32 h-32 md:w-64 md:h-64',
-      mdImageClass: 'w-16 h-16 md:w-32 md:h-32',
-      smImageClass: 'w-4 h-4 md:w-8 md:h-8'
-    }
   }
 })
 </script>
