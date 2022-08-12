14e0cd2c1c6587045f31f4c13f227529133c37db
Matt McArdle <matthew.j.mcardle@gmail.com>
2022-08-12 18:37:41 -0400

    Fix Jest issues (#19)
    * stubbing components
    * Removing optional chaining until I can figure out how to allow it

--- test/MainPage.spec.js
+++ test/MainPage.spec.js
@@ -1,9 +1,16 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import MainPage from '../pages/index'
 
 describe('Main Page', () => {
+  const mount = () => shallowMount(MainPage, {
+    stubs: {
+      NuxtLink: RouterLinkStub,
+      FontAwesomeIcon: true
+    }
+  })
+
   test('is a Vue instance', () => {
-    const wrapper = mount(MainPage)
+    const wrapper = mount()
     expect(wrapper.vm).toBeTruthy()
   })
 })
